39847fc27f90ee0bc0e37be6903f95e66b4538f2
Zachary Baumgarten <zach.baumgarten@yahoo.com>
2022-06-19 10:32:40 -0500

    ples halp

--- src/StylesContext.js
+++ src/StylesContext.js
@@ -1,14 +1,5 @@
 import React from "react";
 
-const StylesContext = React.createContext({
-    background: "#1ECD97",
-    display: "inline-block",
-    width: 120,
-    height: 55,
-    fontSize: 18,
-    letterSpacing: 1,
-    border: "2px solid #1ECD97",
-    borderRadius: 15                   
-});
+const StylesContext = React.createContext({});
 
 export default StylesContext
